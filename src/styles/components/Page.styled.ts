44516ed4fc10114415fb70ef8c1fc918fe1936b4
Jonas <jonasafribeiro@gmail.com>
2024-02-23 12:42:06 -0300

    Corrigida z-index da main para não quebrar animações CSS

--- src/styles/components/Page.styled.ts
+++ src/styles/components/Page.styled.ts
@@ -4,7 +4,7 @@ import styled from "styled-components";
 
 export const StyledMain = styled.main`
   position: relative;
-  z-index: -2;
+  z-index: 0;
   background: #040a2b;
   min-height: calc(100vh - 100px);
 `;
